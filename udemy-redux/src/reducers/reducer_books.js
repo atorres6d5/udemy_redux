b18ee76c1a32f9a44d55827f68f60928e8261ed9
Aldo Torres <atorres6d5@gmail.com>
2018-01-09 23:05:39 -0800

    first commit

---
+++ udemy-redux/src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+const library = ( ) =>{
+  return[
+    {title: "JavaScripts the goodparts"},
+    {title: "Harry Potter"},
+    {title: "The Dark Tower"},
+    {title: "Eloquent Ruby"}
+  ]
+}
+
+export default library
